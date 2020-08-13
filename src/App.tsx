5a882bcd3078ce05551099fdce8e12104e9ce850
mariaeduardaoer <mariaeduardaoer27@gmail.com>
2020-08-13 00:35:52 -0300

    gitignore file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
+function App() { /* App é um componente (uma função que retorna um HTML) */
+  
+  return (
+   <Landing />
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // DOM: árvore de elementos do HTML
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
